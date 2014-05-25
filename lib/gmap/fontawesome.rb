e91a3db3c0ec0543f576c6bc27507fd0fe58de8f
Marino <marinobonetti@gmail.com>
2014-05-25 16:07:11 +0200

    primo commit con generazione gemma

---
+++ lib/gmap/fontawesome.rb
@@ -0,0 +1,8 @@
+require "gmap/fontawesome/version"
+
+module Gmap
+  module Fontawesome
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/gmap/fontawesome/version.rb
@@ -0,0 +1,6 @@
+module Gmap
+  module Fontawesome
+    VERSION = '0.0.1'.freeze
+    FONTAWES_VERSION = '4.1.0'.freeze
+  end
+end
